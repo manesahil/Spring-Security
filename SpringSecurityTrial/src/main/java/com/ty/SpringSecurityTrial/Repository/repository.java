ce1cf1914d2fd34bf78166ebc6f6a397f5551c8a
manesahil <sahilmane3139@gmail.com>
2024-11-12 14:21:54 +0530

    Delete SpringSecurityTrial directory

--- SpringSecurityTrial/src/main/java/com/ty/SpringSecurityTrial/Repository/repository.java
+++
@@ -1,9 +0,0 @@
-package com.ty.SpringSecurityTrial.Repository;
-
-import com.ty.SpringSecurityTrial.model.user;
-
-public interface repository {
-	
-	public user FindUsername(String username);
-
-}
--- SpringSecurityTrial/src/main/java/com/ty/SpringSecurityTrial/Service/service.java
+++
@@ -1,5 +0,0 @@
-package com.ty.SpringSecurityTrial.Service;
-
-public interface service {
-
-}
